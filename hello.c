f98fa3e56ce0069b6c4e204b23a5bc0c72d7774d
Amir <amir.dev21@gmail.com>
2022-04-18 23:52:21 -0400

    hello

--- hello.c
+++ hello.c
@@ -4,5 +4,6 @@
 int main(void)
 {
     string answer = get_string("what is your name? \n");
-    printf("hello, %s, %i\n", answer, 9);
+    string age = get_string("what is your age? \n");
+    printf("hello %s, you are %s\n", answer, age);
 }
